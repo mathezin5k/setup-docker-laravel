ecb9878af8646dbffecffa9fd62699656aa5ff05
mathezin5k <142324331+mathezin5k@users.noreply.github.com>
2023-11-28 18:27:16 -0300

    .

--- routes/web.php
+++ routes/web.php
@@ -27,15 +27,6 @@ Route::resource('cardapio', CardapioController::class);
 Route::resource('availability', AvailabilityController::class);
 
 
-Route::get('/reservas/create', [ReservaController::class, 'create'])->name('reservas.create');
-Route::post('/reservas/store', [ReservaController::class, 'store'])->name('reservas.store');
-Route::get('/reservas', [ReservaController::class, 'index'])->name('reservas.index');
-Route::get('/reservas/{id}', [ReservaController::class, 'show'])->name('reservas.show');
-Route::get('/reservas/{id}/edit', [ReservaController::class, 'edit'])->name('reservas.edit');
-Route::put('/reservas/{id}', [ReservaController::class, 'update'])->name('reservas.update');
-Route::delete('/reservas/{id}', [ReservaController::class, 'destroy'])->name('reservas.destroy');
-
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
